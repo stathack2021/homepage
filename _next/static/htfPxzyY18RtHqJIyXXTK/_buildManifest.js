35c30c4cefe87b7df9c02e8d8099543dc5b081ba
tomoino <tomoino@users.noreply.github.com>
2021-09-12 01:25:09 +0000

    deploy: a81fe459a0c9eb9fa8f1d579fb4aa9754dcd8f90

---
+++ _next/static/htfPxzyY18RtHqJIyXXTK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-45f571bdb6498d6841ba.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
