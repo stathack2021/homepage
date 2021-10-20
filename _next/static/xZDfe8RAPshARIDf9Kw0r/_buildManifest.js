79def6ceb2d4352019c8ae6d54d9c21317316345
tomoino <tomoino@users.noreply.github.com>
2021-10-20 10:15:27 +0000

    deploy: 1215117bb97039220e830adeda2c5610c4969a48

--- _next/static/xZDfe8RAPshARIDf9Kw0r/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9a4eba57305c6b3e17df.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/en":["static/chunks/pages/en-ad8d5731927458f551d6.js"],sortedPages:["/","/_app","/_error","/en"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
