d4b0f534135d619f363d727b31d47b49c1e91a25
dorakemon <51844896+dorakemon@users.noreply.github.com>
2023-01-05 09:39:12 +0900

    add: service-worker

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import useLocale from "../hooks/useLocale"
+import useLocale from '../hooks/useLocale'
 import { Header } from '../modules/header'
 import { About } from '../modules/about'
 import { Value } from '../modules/value'
@@ -9,30 +9,37 @@ import { Member } from '../modules/member'
 import { Company } from '../modules/company'
 import { Contact } from '../modules/contact'
 import { Footer } from '../modules/footer'
+import { useEffect } from 'react'
 
 export const Home = (): JSX.Element => {
-  const locale = "ja";
-  const t = useLocale(locale);
+  const locale = 'ja'
+  const t = useLocale(locale)
+
+  useEffect(() => {
+    navigator.serviceWorker.register('/service-worker.js')
+  }, [])
 
   return (
     <div className="bg-black">
       <Head>
-        <title>{ t.title }</title>
+        <title>{t.title}</title>
         <link rel="icon" href="/favicon.svg" />
-        <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet'></link>
+        <link
+          href="https://fonts.googleapis.com/css?family=Josefin+Sans"
+          rel="stylesheet"
+        ></link>
         <script src="https://sdk.form.run/js/v2/formrun.js"></script>
       </Head>
 
-      <Header locale={ locale }/>
-      <About locale={ locale }/>
-      <Value locale={ locale }/>
-      <Service locale={ locale }/>
-      <News locale={ locale }/>
-      <Member locale={ locale }/>
-      <Company locale={ locale }/>
-      <Contact locale={ locale }/>
-      <Footer locale={ locale }/>
-
+      <Header locale={locale} />
+      <About locale={locale} />
+      <Value locale={locale} />
+      <Service locale={locale} />
+      <News locale={locale} />
+      <Member locale={locale} />
+      <Company locale={locale} />
+      <Contact locale={locale} />
+      <Footer locale={locale} />
     </div>
   )
 }
