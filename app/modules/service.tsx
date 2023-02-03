87257386cf699c4dfe6776b9bdac99c72cc83e3e
hsov9314 <hsov3149@gmail.com>
2023-02-03 19:07:22 +0900

    [update] update example2 section design with figma

--- app/modules/service.tsx
+++ app/modules/service.tsx
@@ -151,52 +151,88 @@ export const Service: React.FC<Props> = (props) => {
 
         <div className="container">
           <div className="flex items-top lg:w-3/5 mx-auto border-b border-gray-200 flex-row">
-            <div className="hidden lg:block">
-              <img src="/images/ex1_1.jpg" alt="example_1" width={'360'} height={'180'} />
-            </div>
-            <div className="flex-grow text-left lg:ml-20 w-20">
+            <div className="flex-grow text-left">
               <p className="text-lg font-en font-bold text-transparent bg-clip-text bg-gradient-to-b from-sh-light-blue  via-sh-blue to-sh-dark-blue">example 01</p>
-              <h2 className="text-3xl md:text-4xl py-2">{ t.service.example.first.title }</h2>
+              <h2 className="text-3xl md:text-4xl py-2">{ t.service.example.second.title }</h2>
               <p className="leading-relaxed text-base font-jp font-medium mb-5">
-                {t.service.example.first.subtitle }
+                {t.service.example.second.subtitle }
               </p>
               <p className="leading-relaxed text-base font-jp font-medium">
-                {t.service.example.first.description}
+                {t.service.example.second.description}
               </p>
             </div>
+            <div className="hidden lg:block lg:ml-20">
+              <img src="/images/ex2_1.jpg" alt="example_2" width={'353'} height={'299'} />
+            </div>
+            
             <div className="lg:hidden w-full py-5">
-                <img className="w-full" src="/images/ex1_1.jpg" alt="example_1" width={'352'} height={'300'} />
+                <img className="w-full" src="/images/ex2_1.jpg" alt="example_1" width={'352'} height={'300'} />
               </div>
           </div>
-          <div className="flex items-top my-10 lg:w-3/5 mx-auto border-b border-gray-200 flex-row">
-            <div className="flex flex-col text-center items-center">
-              <div className="inline-flex items-center justify-center rounded-full bg-blue-100 text-sh-blue mb-4 flex-shrink-0">
-                <img src="/images/ex1_2.jpg" alt="example_1" width={'440'} height={'220'} />
+          <div className="flex flex-wrap items-top my-10 lg:w-3/5 mx-auto border-b border-gray-200 flex-row">
+            <div className="flex flex-col text-center items-center w-1/2">
+              <div className="inline-flex items-center justify-center mb-4 flex-shrink-0">
+                <img src="/images/ex2_2.png" alt="example_1" width={'440'} height={'220'} />
               </div>
               <div className="flex-grow pl-6">
-                <h2 className="text-gray-900 font-jp text-lg title-font font-bold mb-2 text-left">{ t.service.example.first.content.first.title }</h2>
+                <h2 className="text-gray-900 font-jp text-lg title-font font-bold mb-2 text-left">{ t.service.example.second.content.first.title }</h2>
                 <p className="leading-relaxed text-base font-jp font-medium text-left">
-                { t.service.example.first.content.first.description }
+                { t.service.example.second.content.first.description }
                 </p>
               </div>
             </div>
-            <div className="flex flex-col text-center items-center">
+            <div className="flex flex-col text-center items-center w-1/2">
               <div className="inline-flex items-center justify-center text-sh-blue mb-4 flex-shrink-0">
-                <img src="/images/ex1_3.png" alt="example_1" width={'440'} height={'220'} />
+                <img src="/images/ex2_3.png" alt="example_1" width={'440'} height={'220'} />
               </div>
               <div className="flex-grow pl-6">
-                <h2 className="text-gray-900 font-jp text-lg title-font font-bold mb-2 text-left">{ t.service.example.first.content.second.title }</h2>
+                <h2 className="text-gray-900 font-jp text-lg title-font font-bold mb-2 text-left">{ t.service.example.second.content.second.title }</h2>
                 <p className="leading-relaxed text-base font-jp font-medium text-left">
-                { t.service.example.first.content.second.description }
+                { t.service.example.second.content.second.description }
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col text-center items-center w-1/2 mt-6">
+              <div className="inline-flex items-center justify-center text-sh-blue mb-4 flex-shrink-0">
+                <img src="/images/ex2_4.png" alt="example_1" width={'440'} height={'220'} />
+              </div>
+              <div className="flex-grow pl-6">
+                <h2 className="text-gray-900 font-jp text-lg title-font font-bold mb-2 text-left">{ t.service.example.third.content.second.title }</h2>
+                <p className="leading-relaxed text-base font-jp font-medium text-left">
+                { t.service.example.third.content.second.description }
                 </p>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="p-2 w-full mt-10 ">
-          <button onClick={() => { window.location.href = '/#contact' }} className="flex mx-auto w-48 md:w-96 h-12 md:h-16 items-center justify-center font-bold text-white bg-gradient-to-l from-sh-light-blue  via-sh-blue to-sh-dark-blue hover:bg-gradient-to-l hover:from-sh-light-blue-hover  hover:via-sh-blue-hover hover:to-sh-dark-blue-hover  border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">CONTACT</button>
+        <div className="lg:w-3/4 mx-auto px-2 font-jp font-medium text-sh-black">
+          <div className="h-full bg-sh-blue bg-opacity-20 p-4 rounded-xl">
+            <div className="flex flex-wrap divide-x divide-sh-gray3 py-10">
+              <div className="flex w-1/2 items-center justify-center">
+                <div className="mr-5">
+                  <img src="/images/document.svg" width={120} height={120} />
+                </div>
+                <div className="flex flex-col items-center ml-5">
+                    <button className="mx-auto w-80 md:w-80 h-12 md:h-16 font-bold text-white bg-sh-blue hover:bg-sh-blue-hover  border-0 py-2 px-8 focus:outline-none rounded-full text-lg">{ t.service.contact.document.button}</button>
+                    <p className="text-base font-jp font-medium mt-4">{ t.service.contact.document.description }</p>
+                </div>
+              </div>
+
+              <div className="flex w-1/2 items-center justify-center">
+                <div className="mr-5">
+                  <img src="/images/ai.svg" width={120} height={120} />
+                </div>
+                <div className="flex flex-col items-center ml-5">
+                    <button className="mx-auto w-80 md:w-80 h-12 md:h-16 font-bold text-sh-blue bg-sh-white hover:bg-sh-blue-hover border-2 border-sh-blue py-2 px-8 focus:outline-none rounded-full text-lg">{ t.service.contact.ai.button}</button>
+                    <p className="text-base font-jp font-medium mt-4">{ t.service.contact.ai.description }</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
+        
 
         </div>
       </div>
